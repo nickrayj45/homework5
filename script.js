fbc7f4f9d82a881d223597bea706e04f2a93ea73
Nicholas Jaquez <nrjaquez@gmail.com>
2020-01-29 23:20:55 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// I need to learn how to use Moments.js to reflect the date and the time on the page
+// I need to make these buttons activate once they are clicked
+// Once these buttons are clicked and the user types them in, the information needs to
+// be saved and stored
