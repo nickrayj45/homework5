0660f3e97fc0b2c4d8b76f70fb384fd838919c03
Nicholas Jaquez <nrjaquez@gmail.com>
2020-01-31 23:23:48 -0800

    commit w/adjustments

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-// I need to learn how to use Moments.js to reflect the date and the time on the page
-// I need to make these buttons activate once they are clicked
-// Once these buttons are clicked and the user types them in, the information needs to
-// be saved and stored
+// Variables
+
+// Variables for the 8 different hours that will be used
+var btn9 = document.querySelector("#btn9");
+var btn10 = document.querySelector("#btn10");
+var btn11 = document.querySelector("#btn11");
+var btn12 = document.querySelector("#btn12");
+var btn1 = document.querySelector("#btn1");
+var btn2 = document.querySelector("#btn2");
+var btn3 = document.querySelector("#btn3");
+var btn4 = document.querySelector("#btn4");
+
+// Create a function to collect the data that the user will input
+function userInfo() {
+  btn9.innerHTML = "";
+}
