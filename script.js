47d209eea36c2967d892ee98199ee7e17a2a9cac
Nicholas Jaquez <nrjaquez@gmail.com>
2020-02-02 23:09:58 -0800

    commit w/bootstrap adjustments

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 // Variables
 
-// Variables for the 8 different hours that will be used
-var btn9 = document.querySelector("#btn9");
-var btn10 = document.querySelector("#btn10");
-var btn11 = document.querySelector("#btn11");
-var btn12 = document.querySelector("#btn12");
-var btn1 = document.querySelector("#btn1");
-var btn2 = document.querySelector("#btn2");
-var btn3 = document.querySelector("#btn3");
-var btn4 = document.querySelector("#btn4");
+// Variable to collect
+var userInfo = document.querySelector("btn9");
 
-// Create a function to collect the data that the user will input
-function userInfo() {
-  btn9.innerHTML = "";
-}
+// for loop that makes current time color-coded
+
+// grab info from user
+btn9.textContent = userInfo;
+
+// Function to save user info once they click on it
+$("#btn9").click(function() {
+  var userInfo = $(this).text();
+  $("#btn9").val(userInfo);
+});
+
+// localStorage.setItem("can only be a string")
