126a0d3b4a1f2d8cb221d5abea67c2caa53f3323
Nicholas Jaquez <nrjaquez@gmail.com>
2020-02-04 23:11:21 -0800

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# homework5
-Homework5
+With this planner, you will be able to type your plans at any hour between 9:00 a.m. and 4 p.m.
+
+To start, you can type your plans in the input field and then click "Add" once you are done.
+
+You can refresh the page to see the current date and local time.
+
+The current time will be shown in yellow and the future will be shown in green. Here you can book important things you need to do throughout the day. The past will be shown in red indicating that the time has passed. Before 9 a.m., everything should be green and after 4 p.m., everything will be red.
--- script.js
+++ script.js
@@ -1,44 +1,56 @@
 // Variables
 
-// Variable to collect
-// var userInfo = document.querySelector("btn9");
 var currentDate = moment();
-var currentTime = moment().format("dddd, MMMM, Do YYYY, h:mm:ss a");
-$("#currentTime").text(moment);
-console.log(currentTime);
+var currentTime = moment().hour();
+var currentHour = moment().format("LTS");
+$("#currentTime").text(currentHour);
+// console.log("current:" + currentTime);
 
-// for loop that makes current time color-coded
+// if statements that makes current time color-coded
 $(".timenow").each(function() {
   var hourTime = parseInt($(this).attr("id"));
-  console.log(hourTime);
+  // console.log("hour:" + hourTim`e);
+  // if (true) {
+  // console.log(currentTime);
+  // console.log(hourTime);
+  // console.log(this);
+  if (currentTime > hourTime) {
+    $(this).css("background", "red");
+
+    // console.log("past");
+  }
   if (currentTime === hourTime) {
-    addedClass = "yellow";
-    console.log("past");
+    $(this).css("background", "yellow");
+    // console.log("current");
+  }
+  if (currentTime < hourTime) {
+    $(this).css("background", "green");
+    // console.log("future");
   }
 });
 
 // Function to save user info once they click on it
 $("#btn-9").click(function() {
   var userInfo = $("#btn9").val();
-  console.log(userInfo);
+  // console.log(userInfo);
   localStorage.setItem("9AM", userInfo);
 });
 
 $("#btn-10").click(function() {
   var userInfo = $("#btn10").val();
-  console.log(userInfo);
+  // console.log(userInfo);
   localStorage.setItem("10AM", userInfo);
 });
 
 $("#btn-11").click(function() {
   var userInfo = $("#btn11").val();
-  console.log(userInfo);
+  // console.log(userInfo);
   localStorage.setItem("11AM", userInfo);
 });
 
 $("#btn-12").click(function() {
   var userInfo = $("#btn12").val();
-  console.log(userInfo);
+  // console.log(userInfo);
   localStorage.setItem("12PM", userInfo);
 });
 
@@ -56,12 +68,12 @@ $("#btn-2").click(function() {
 
 $("#btn-3").click(function() {
   var userInfo = $("#btn-3").val();
-  console.log(userInfo);
+  // console.log(userInfo);
   localStorage.setItem("2PM", userInfo);
 });
 
 $("#btn-4").click(function() {
   var userInfo = $("#btn-4").val();
-  console.log(userInfo);
+  // console.log(userInfo);
   localStorage.setItem("4PM", userInfo);
 });
